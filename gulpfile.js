269c8f79ac3f5da397744485eaad5ff8f893e32f
Rodrigo Viturino <Viturino_souza@outlook.com>
2020-01-10 11:15:34 -0300

    Header e Footer

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+sass.compiler = require('node-sass');
+
+function compilaSass(){
+    return gulp.src(['./assets/scss/*.scss'])
+        .pipe(sass({
+            // outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream())
+};
+
+function javascript(){
+    return gulp.src(['./node_modules/bootstrap/dist/js/bootstrap.min.js'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./assets/js/'))
+        .pipe(browserSync.stream())
+};
+
+function browser(){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+};
+
+function watch(){
+    gulp.watch(['./*.html']).on('change', browserSync.reload);
+    gulp.watch(['./template/*.html', './template/**/*.html']).on('change', browserSync.reload)
+    gulp.watch(['./template-parts/*.html', './template-parts/*/*.html']).on('change', browserSync.reload);
+    gulp.watch(['./assets/scss/*.scss', './assets/scss/**/*.scss'], compilaSass);
+    // gulp.watch('./assets/scss/**/*.scss', 'compilaSass');
+    gulp.watch('./assets/js/*.js', javascript);
+    gulp.watch('./assets/js/**/*.js', javascript);
+};
+
+gulp.task('default', gulp.parallel([compilaSass, javascript, browser,watch]));
