7b53a291e5a994a8eb607304268298132b1be822
Rodrigo Viturino <viturino_souza@outlook.com>
2020-01-31 14:45:08 -0300

    Alterações internas em html e css

---
+++ bkp.md
@@ -0,0 +1,107 @@
+menu navbar HTML
+<!-- <nav class="header__rowBottom__nav col-md-8 col-2">
+                    <ul class="header__rowBottom__nav__menu">
+                        <li class="menu-close">
+                            <i class="fas fa-times fa-2x"></i>
+                        </li>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Company</a>
+                        </li>
+                        <li>
+                            <a href="#">Cases</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                        <li>
+                            <a href="#">Pages</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                    <div class="menu-burgue">
+                        <i class="fas fa-th fa-2x"></i>
+                    </div>
+                </nav> -->
+
+menu navbar  CSS
+   // &__nav {
+
+        //     &__menu {
+        //         display: flex;
+        //         margin: 0;
+
+        //         @include media-breakpoint-only(xs) { 
+        //             position: fixed;
+        //             top: -10px;
+        //             right: -600px;                
+        //             transition: all 0.7s;
+        //         }
+
+        //         .menu-close {
+        //             display: none;
+
+        //         @include media-breakpoint-only(xs) { 
+        //             display: block;
+        //             position: absolute;
+        //             top: 10px;
+        //             right: 80px;
+        //             color: #fff;
+        //         }
+
+        //         }
+
+        //         li {
+        //             a {
+        //                 margin-right: 40px;
+        //                 color: white;
+        //                 text-transform: uppercase;
+        //                 text-decoration: none;
+
+        //                 &:hover {
+        //                     color: #ccc;
+        //                 }
+        //             }
+        //         }
+        //     }
+
+        //     .menu-burgue{
+        //         display: none;
+        //         @include media-breakpoint-only(xs) {
+        //             display: block;
+        //             opacity: 1;
+        //             color: #fff;
+        //             i {
+        //                 cursor: pointer;
+        //             }
+        //         }
+                
+        //     }
+
+        // }
+
+Menu Burgue
+   // .menuOpen {
+    //     position: fixed;
+    //     background-color: gray;
+    //     top: 0px;
+    //     z-index: 11;
+    //     right: -63px;
+    //     display: flex !important;
+    //     align-items: center;
+    //     flex-direction: column !important;
+    //     justify-content: space-around;
+    //     height: 100vh;
+    //     width: 100%;
+
+    //     .menu-burgue {
+    //         display: none !important;
+    //     }
+    // }
--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,145 @@
+'use strict';
+
+// Load Plugins
 const gulp = require('gulp');
-const minifyJS = require('gulp-uglify');
-const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const rollup = require('gulp-better-rollup');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
 const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
+const htmlmin = require('gulp-html-minifier');
+const mustache = require('gulp-mustache');
+const version = require('gulp-version-number');
+const sitemap = require('gulp-sitemap');
+const pretty = require('gulp-pretty-data');
 
-sass.compiler = require('node-sass');
+// Mustache
+function html_render() {
+    return gulp.src("src/html/*.html")
+        .pipe(mustache('./src/html/variables.json', {}, {})) // mustache(view, options, partials)
+        .pipe(version({
+            'value': '%MDS%',
+            'append': {
+                'key': 'v',
+                'to': ['css', 'js', 'jpg', 'png']
+            }
+        }))
+        .pipe(gulp.dest("./assets/html"));
+}
 
-function compilaSass(){
-    return gulp.src(['./assets/scss/*.scss'])
+// HTML
+function html_minifier() {
+    return gulp.src('src/page/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeEmptyAttributes: true,
+            sortAttributes: true,
+            sortClassName: true
+        }))
+        .pipe(gulp.dest('./public'));
+}
+
+// Styles
+function styles() {
+    return gulp.src(['src/scss/theme.scss'])
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(sass({
-            // outputStyle: 'compressed'
-        }))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(concat('style.css'))
-        .pipe(gulp.dest('./'))
-        .pipe(browserSync.stream())
-};
+            includePaths: [
+                './node_modules/bootstrap/scss/',
+                './node_modules/@fortawesome/'
+            ]
+        }).on('error', sass.logError))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('./'))
+        .pipe(browserSync.stream({ match: 'src/scss/**/*.scss' }))
+        .pipe(gulp.dest('./public'))
+        // .pipe(browserSync.stream());
+}
 
-function javascript(){
-    return gulp.src(['./node_modules/bootstrap/dist/js/bootstrap.min.js'],['./assets/js/*.js'])
-        .pipe(minifyJS())
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./assets/js/'))
-        .pipe(browserSync.stream());
-};
+function styles_login() {
+    return gulp.src('src/scss/login.scss')
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sass({
+            includePaths: [
+                './node_modules/bootstrap/scss/',
+                './node_modules/@fortawesome/'
+            ]
+        }).on('error', sass.logError))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('./'))
+        .pipe(browserSync.stream({ match: 'src/scss/**/*.scss' }))
+        .pipe(gulp.dest('./assets'));
+}
 
-function browser(){
+// Scripts
+function scripts() {
+    return gulp.src(['src/js/**/*.js'])
+        .pipe(sourcemaps.init())
+        .pipe(rollup({ plugins: [resolve(), commonjs()] }, 'umd'))
+        .pipe(babel({ presets: ['@babel/env'] }))
+        .pipe(concat('theme.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(browserSync.stream({ match: 'src/js/**/*.js' }))
+        .pipe(gulp.dest('./public'));
+}
+
+// Images
+function images() {
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(sourcemaps.write('./'))
+        .pipe(browserSync.stream({ match: 'src/images/*' }))
+        .pipe(gulp.dest('./public/images'));
+}
+
+// Fonts
+function fonts() {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(sourcemaps.write('./'))
+        .pipe(browserSync.stream({ match: 'node_modules/@fortawesome/fontawesome-free/webfonts/*' }))
+        .pipe(gulp.dest('./public/fonts'));
+}
+
+// Default task
+gulp.task('default', gulp.series(html_minifier, styles, scripts, images, fonts));
+
+// Watch task
+gulp.task('watch', function() {
     browserSync.init({
         server: {
-            baseDir: './'
+            baseDir: './public',
+            index: 'index.html',
+            serveStaticOptions: {
+                extensions: ['html']
+            }
         }
     });
-};
 
-function watch(){
-    gulp.watch(['./*.html']).on('change', browserSync.reload);
-    gulp.watch(['./template/*.html', './template/**/*.html']).on('change', browserSync.reload)
-    gulp.watch(['./template-parts/*.html', './template-parts/*/*.html']).on('change', browserSync.reload);
-    gulp.watch(['./assets/scss/*.scss', './assets/scss/**/*.scss'], compilaSass);
-    gulp.watch('./assets/js/*.js');
-    // gulp.watch('./assets/scss/**/*.scss', 'compilaSass');
-};
+    // gulp.watch(['src/*.html', 'src/variables.json'], html_render).on('change', browserSync.reload);
+    // gulp.watch('src/templates/*.mustache', html_render).on('change', browserSync.reload);
+    gulp.watch('src/page/*.html', html_minifier).on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js', scripts).on('change', browserSync.reload);
+    gulp.watch('src/scss/**/*.scss', styles).on('change', browserSync.reload);
+    // gulp.watch('src/scss/**/*.scss', styles_login).on('change', browserSync.reload);
+    gulp.watch('src/images/*', images).on('change', browserSync.reload);
+    gulp.watch('node_modules/@fortawesome/fontawesome-free/webfonts/*', fonts).on('change', browserSync.reload);
+});
 
-gulp.task('default', gulp.parallel([compilaSass, javascript, browser,watch]));
+gulp.task('dev', function() {
+    gulp.watch('src/js/**/*.js', scripts);
+    gulp.watch('src/scss/**/*.scss', styles);
+    // gulp.watch('src/scss/**/*.scss', styles_login);
+    gulp.watch('src/images/*', images);
+    gulp.watch('node_modules/@fortawesome/fontawesome-free/webfonts/*', fonts);
+});
